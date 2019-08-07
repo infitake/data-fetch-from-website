b2ab0714aff98b448821b5e9e81d77abe795aac1
infitake <rvndryadv@gmail.com>
2019-08-08 00:39:07 +0530

    add json file for target study college data

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -3,19 +3,24 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = 'law-college'
-    page_no = 2
+    page_no = 20
     start_urls = [
-        'https://nodesk.co/remote-companies/',
+        "https://targetstudy.com/colleges/colleges-in-delhi.html?recNo=10",
     ]
 
     def parse(self, response):
-        for college in response.css(".b--blue"): 
+        for college in response.css(".panel.panel-custom"): 
             yield {
                 
-                'Company Name':college.css('.mt3::text').get(),
-                'Description':college.css('.mt0::text').get(),
-                'Logo': college.css("img::attr(data-src)").get(),
+                'link': college.css(".heading1::attr(href)").get(),
+                # 'Company Name':college.css('.mt3::text').get(),
+                # 'Description':college.css('.mt0::text').get(),
+                # 'Logo': college.css("img::attr(data-src)").get(),
                 }
+            next_page="https://targetstudy.com/colleges/colleges-in-delhi.html?recNo="+str(QuotesSpider.page_no)
+            if QuotesSpider.page_no < 1100:
+                QuotesSpider.page_no += 10
+                yield response.follow(next_page, self.parse)
                 
 
  
